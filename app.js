9c70b3b1c614eb6d801e1b2d3623d921691a411e
William and Lady Navas <ladysantoyo@Williams-MacBook-Pro.local>
2020-01-10 23:09:33 -0500

    logo

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ mongoose.connect(mdb, { useNewUrlParser: true })
 var db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'Nuevavida7',
     database: 'blog'
 });
 
